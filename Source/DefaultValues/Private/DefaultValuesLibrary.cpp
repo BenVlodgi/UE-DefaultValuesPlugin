cfae0c18218cce5f7977b2b39daa6d942fb76d82
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2022-03-29 10:01:27 -0400

    Reduced Cost, and expand supported types by replacing C++ functions with Macros
    Code module removed.
    Now this is a content plugin.
    Macro Library holds two macros
    - Default
    - Default Scale
    The new Default Macro covers all allowed types via wildcard, which supports structs too.
    Also benefit to this is that it is no longer a function call to get a hardcoded value. Macro is collapsed into the blueprint. This should add virtually no cost, and be cheaper than the function call.

--- Source/DefaultValues/Private/DefaultValuesLibrary.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Dream Seed LLC, 2022
-
-#include "DefaultValuesLibrary.h"
-#include "DefaultValues.h"
-
-UDefaultValuesLibrary::UDefaultValuesLibrary(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) { }
